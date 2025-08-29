b04bad352b714c9401daa87f75d58d82bc1ed5d6
Rohith <rohith.kontham2803@gmail.com>
2025-08-29 19:20:00 +0530

    frontend

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 import Home from "./components/Home";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/*" element={<Home />}/>
+      
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
